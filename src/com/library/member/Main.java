6bfa8215235611d0d37d2d689acbc922f6439ea4
helendoan <helendoan1907@gmail.com>
2018-09-11 23:14:35 -0500

    Add MemberController

---
+++ src/com/library/member/Main.java
@@ -0,0 +1,23 @@
+package com.library.member;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("MemberController.fxml"));
+        primaryStage.setTitle("Add Member");
+        primaryStage.setScene(new Scene(root, 800, 375));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
